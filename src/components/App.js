4e2bd14992d12df268c7117365821caa85ac264a
박은지 <eunji@bag-eunjiui-MacBookPro.local>
2019-10-30 00:37:06 +0900

    react review repo

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React , { Component } from 'react';
+import '../css/App.css';
+
+
+class App extends Component {
+  componentDidMount(){
+
+  }
+  render() {
+    console.log('rendering');
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
